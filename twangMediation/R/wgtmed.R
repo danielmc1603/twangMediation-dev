ffcb2a700ddcdb57cb6cdd21e8110ce68c1d74e8
katcast <katcast@users.noreply.github.com>
2021-04-09 14:55:46 -0400

    Update wgtmed.R with twang call and fixed renaming ps model results
    Added "twang:::" for twang functions and changed renaming for "model_a" to renaming for "model_a_res" (as that is what it is called in the function) and likewise for model_m0 and model_m1.

--- twangMediation/R/wgtmed.R
+++ twangMediation/R/wgtmed.R
@@ -163,7 +163,7 @@ wgtmed <- function(formula.med,
     			if(total_effect_ps$estimand != "ATE") stop("Total effect must be ATE. Estimand in total_effect_ps != 'ATE'")
     			if(length(total_effect_stop_rule) > 1) warning("Multiple stopping rules provided for total_effect_ps. Only the first is used")
     			if(length(total_effect_stop_rule) == 0) stop("A stopping rule must be specified for the total effects. total_effect_stop_rule must be specified.") 
-    			total_effect_wts <- get.weights(total_effect_ps, total_effect_stop_rule[1])
+    			total_effect_wts <- twang:::get.weights(total_effect_ps, total_effect_stop_rule[1])
     			var.names.tx <- total_effect_ps$gbm.obj$var.names
     }
 
@@ -254,7 +254,7 @@ wgtmed <- function(formula.med,
           n.keep = ps_n.keep, n.grid = ps_n.grid))
     }
     #* Get p(A|X) using same covariates and stopping rules as for mediation model 
-    model_a_res <- do.call(ps, c(list(data = data, estimand = "ATE"), 
+    model_a_res <- do.call(twang:::ps, c(list(data = data, estimand = "ATE"), 
           ps_args))
 
     #* Calculate w_10 weights
@@ -262,7 +262,7 @@ wgtmed <- function(formula.med,
     #* So we run ps with "tx" = 1- tx and ATT and the estimand
     ps_args$formula <- as.formula(paste("a_treatment0~", paste(c(m_mediators, var.names.med), collapse="+"))) 
     data[,"a_treatment0"] <- 1 - data[,a_treatment]
-    model_m0_res <- do.call(ps, c(list(data = data, estimand = "ATT"), 
+    model_m0_res <- do.call(twang:::ps, c(list(data = data, estimand = "ATT"), 
       ps_args))
     
     #~ Remove a_treatment0 variable in data
@@ -281,7 +281,7 @@ wgtmed <- function(formula.med,
     #* Note p(A = 1 | M, X)/p(A = 0 | M, X) are the "ATT" weights for the crtl group when estimate = ATT
     #* So we run ps with ATT and the estimand
     ps_args$formula <- as.formula(paste(a_treatment,"~", paste(c(m_mediators, var.names.med), collapse="+"))) 
-    model_m1_res <- do.call(ps, c(list(data = data, estimand = "ATT"), 
+    model_m1_res <- do.call(twang:::ps, c(list(data = data, estimand = "ATT"), 
       ps_args))
 
     #~  1/p(A=1 | X)
@@ -303,15 +303,15 @@ wgtmed <- function(formula.med,
     } 
 
   ##remove ATE and ATT from names 
-  names(model_a$desc) <- gsub(".ATE","",names(model_a$desc))
-  names(model_m0$desc) <- gsub(".ATT","",names(model_m0$desc))
-  names(model_m1$desc) <- gsub(".ATT","",names(model_m1$desc))
-  names(model_a$ps) <- gsub(".ATE","",names(model_a$ps))
-  names(model_m0$ps) <- gsub(".ATT","",names(model_m0$ps))
-  names(model_m1$ps) <- gsub(".ATT","",names(model_m1$ps))
-  names(model_a$w) <- gsub(".ATE","",names(model_a$w))
-  names(model_m0$w) <- gsub(".ATT","",names(model_m0$w))
-  names(model_m1$w) <- gsub(".ATT","",names(model_m1$w))
+  names(model_a_res$desc) <- gsub(".ATE","",names(model_a_res$desc))
+  names(model_m0_res$desc) <- gsub(".ATT","",names(model_m0_res$desc))
+  names(model_m1_res$desc) <- gsub(".ATT","",names(model_m1_res$desc))
+  names(model_a_res$ps) <- gsub(".ATE","",names(model_a_res$ps))
+  names(model_m0_res$ps) <- gsub(".ATT","",names(model_m0_res$ps))
+  names(model_m1_res$ps) <- gsub(".ATT","",names(model_m1_res$ps))
+  names(model_a_res$w) <- gsub(".ATE","",names(model_a_res$w))
+  names(model_m0_res$w) <- gsub(".ATT","",names(model_m0_res$w))
+  names(model_m1_res$w) <- gsub(".ATT","",names(model_m1_res$w))
   }
   
   if(method!="ps") {
