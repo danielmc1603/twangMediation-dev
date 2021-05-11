81802e0c84f31beb5fc8ea93dba185cea680197a
danielmc1603 <45241123+danielmc1603@users.noreply.github.com>
2021-05-11 12:37:47 -0400

    Update print.bal.table.mediation.R

--- twangMediation/R/print.bal.table.mediation.R
+++ twangMediation/R/print.bal.table.mediation.R
@@ -1,5 +1,14 @@
+#' Default print statement for `mediation` class
+#'
+#' @param object A `bal.table.mediation` object
+#' @param ... Additional arguments.
+#'
+#' @method print bal.table.mediation
+#' @export
 print.bal.table.mediation <- function(object,...){
 
+if(class(object) != "bal.table.mediation"){stop("object must be of class bal.table.mediation")}
+  
 # Print the note
 cat(attr(object,"note"))
 
