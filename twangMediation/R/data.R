91999ed5ce9cb25cf6d0b6a4c2bafaef7e45f0c7
Tophey <hz1954@nyu.edu>
2021-06-22 17:34:54 -0400

    Added the data and the documentation.

---
+++ twangMediation/R/data.R
@@ -0,0 +1,18 @@
+# #' Substance use data among women from 
+# #' the National Survey of Drug Use and Health
+#'
+#' A dataset containing the substance use condition and sexual orientation 
+#' of 40293 women.
+#' .
+#'
+#' @format A data frame with 40293 rows and 24 variables:
+#' \describe{
+#'   \item{lgb_flag}{whether or not belong to LGB, binary variable}
+#'   \item{cig15}{early smoking initiating at 15 years old}
+#'   \item{cigmon}{current smoker when taking the survey}
+#'   ...
+#' }
+#' @source \url{https://nsduhweb.rti.org/respweb/homepage.cfm}
+"NSDUH_female"
+
+
