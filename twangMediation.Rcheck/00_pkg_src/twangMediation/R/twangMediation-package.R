2fb66901f3249ef4da2e26a9d2296749d39fb005
Haoyu Zhou <hz1954@nyu.edu>
2022-07-20 11:51:01 -0400

    Updated version number and deleted check files

--- twangMediation.Rcheck/00_pkg_src/twangMediation/R/twangMediation-package.R
+++
@@ -1,7 +0,0 @@
-#' @keywords propensity score
-#' @import lattice
-#' @import latticeExtra
-#' @import survey
-#' @importFrom graphics par boxplot
-#' @importFrom stats aggregate as.formula glm na.omit na.pass predict qnorm update
-"_PACKAGE"
